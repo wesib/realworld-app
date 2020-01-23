4aa7b5474b3a1c77524df6cd428f96e4415ea5c0
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-23 14:32:17 +0700

    Authentication status

--- src/common/layout/container.component.ts
+++ src/common/layout/container.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@wesib/wesib';
+import { Component, ComponentContext, DefaultNamespaceAliaser, ElementRender, Render } from '@wesib/wesib';
+import { css__naming, QualifiedName } from 'namespace-aliaser';
+import { AuthService, AuthUser } from '../auth';
 import { Conduit__NS } from '../conduit.ns';
 import { MainComponent } from './main.component';
 import { NavbarComponent } from './navbar.component';
+
+const authenticatedClassQName: QualifiedName = ['authenticated', Conduit__NS];
+const notAuthenticatedClassQName: QualifiedName = ['not-authenticated', Conduit__NS];
 
 @Component({
   name: ['container', Conduit__NS],
@@ -10,4 +15,46 @@ import { NavbarComponent } from './navbar.component';
   },
 })
 export class ContainerComponent {
+
+  private _user?: AuthUser;
+
+  constructor(private readonly _context: ComponentContext) {
+    _context.whenOn(connectSupply => {
+      _context.get(AuthService).user.tillOff(connectSupply)((user?, _failure?) => {
+        this.user = user;
+      });
+    });
+  }
+
+  get user(): AuthUser | undefined {
+    return this._user;
+  }
+
+  set user(value: AuthUser | undefined) {
+
+    const oldValue = this._user;
+
+    this._user = value;
+    this._context.updateState('user', value, oldValue);
+  }
+
+  @Render()
+  render(): ElementRender {
+
+    const nsAlis = this._context.get(DefaultNamespaceAliaser);
+    const authenticatedClass = css__naming.name(authenticatedClassQName, nsAlis);
+    const notAuthenticatedClass = css__naming.name(notAuthenticatedClassQName, nsAlis);
+    const { classList }: Element = this._context.element;
+
+    return () => {
+      if (this.user) {
+        classList.remove(notAuthenticatedClass);
+        classList.add(authenticatedClass);
+      } else {
+        classList.remove(authenticatedClass);
+        classList.add(notAuthenticatedClass);
+      }
+    };
+  }
+
 }
--- src/template/conduit-navbar.ts
+++ src/template/conduit-navbar.ts
@@ -7,18 +7,18 @@ Handlebars.registerPartial(
   <div class="container">
     <a class="navbar-brand" href="">conduit</a>
     <ul class="nav navbar-nav pull-xs-right">
-      <li class="nav-item">
+      <li class="nav-item if-authenticated@conduit">
         <a class="nav-link" href="editor/">
           <i class="ion-compose"></i>&nbsp;New Post
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item unless-authenticated@conduit">
         <a class="nav-link" href="login/">Sign in</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item unless-authenticated@conduit">
         <a class="nav-link" href="register/">Sign up</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item if-authenticated@conduit">
         <a class="nav-link" href="settings/">Settings</a>
       </li>
     </ul>
