1e38aeee401ba313a265e9f4963a788df0ac4940
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-23 13:50:51 +0700

    Add login page

---
+++ src/pages/login/index.ts
@@ -0,0 +1,4 @@
+import { conduitContext } from '../../common';
+import { LoginComponent } from './login.component';
+
+conduitContext.load(LoginComponent);
---
+++ src/pages/login/login.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@wesib/wesib';
+import { Conduit__NS } from '../../common';
+
+@Component(['login', Conduit__NS])
+export class LoginComponent {
+}
