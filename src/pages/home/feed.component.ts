d46c65ca29dd801c94d93a57e135490c8fa9584c
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-02-28 17:09:20 +0700

    `@RenderFeed` decorator

--- src/pages/home/feed.component.ts
+++ src/pages/home/feed.component.ts
@@ -1,14 +1,11 @@
-import { HierarchyContext, Navigation } from '@wesib/generic';
-import { BootstrapWindow, Component, ComponentContext, ElementRenderer, Render } from '@wesib/wesib';
-import { nextOnEvent, trackValue } from 'fun-events';
+import { Navigation } from '@wesib/generic';
+import { Component, ComponentContext } from '@wesib/wesib';
 import { Conduit__NS } from '../../common';
-import { ApiResponse } from '../../common/api';
-import { ArticleList, FeedRequest, feedRequestsEqual, FeedService } from '../../common/feed';
-import { ApiErrorGenerator } from '../../common/input';
+import { FeedRequest } from '../../common/feed';
 import { ArticleListComponent } from './article-list.component';
-import { FeedArticleList } from './feed-article-list';
 import { FeedPagerComponent } from './feed-pager.component';
 import { PageFeedParam } from './page-feed-param';
+import { RenderFeed } from './render-feed.decorator';
 
 @Component(
     ['feed', Conduit__NS],
@@ -23,90 +20,18 @@ import { PageFeedParam } from './page-feed-param';
 )
 export class FeedComponent {
 
-  private readonly _request = trackValue<FeedRequest>({});
-  private _response = trackValue<ApiResponse<ArticleList>>();
+  @RenderFeed()
+  request: FeedRequest = {};
 
-  constructor(private readonly _context: ComponentContext) {
+  constructor(context: ComponentContext) {
 
-    const navigation = _context.get(Navigation);
-    const feedService = _context.get(FeedService);
+    const navigation = context.get(Navigation);
 
-    this._response.on(
-        (newResponse, oldResponse) => _context.updateState('response', newResponse, oldResponse),
-    );
-    this._context.get(HierarchyContext).provide({
-      a: FeedArticleList,
-      is: this._response.read.keep.thru_(
-          response => response?.ok
-              ? response.body
-              : { articles: [], articlesCount: 0 },
-      ),
-    });
-    _context.whenOn(supply => {
+    context.whenOn(supply => {
       navigation.read.tillOff(supply)(page => {
-
-        const request = page.get(PageFeedParam);
-        const prevRequest = this._request.it;
-
-        if (!feedRequestsEqual(request, prevRequest)) {
-          this._request.it = request;
-        }
+        this.request = page.get(PageFeedParam);
       });
-      this._request.read
-          .tillOff(supply)
-          .thru_(
-              request => {
-                this._response.it = undefined;
-                return nextOnEvent(feedService.articles(request));
-              },
-          )(response => this._response.it = response);
     });
-  }
-
-  @Render()
-  render(): ElementRenderer {
-
-    const { contentRoot }: { contentRoot: Node } = this._context;
-    const document = this._context.get(BootstrapWindow).document;
-    const errorGen = this._context.get(ApiErrorGenerator);
-    const list = contentRoot.appendChild(document.createElement('conduit-article-list'));
-    const pager = contentRoot.appendChild(document.createElement('conduit-feed-pager'));
-    const range = document.createRange();
-
-    range.setStartAfter(list);
-    range.setEndBefore(pager);
-
-    return () => {
-      range.deleteContents();
-
-      const response = this._response.it;
-
-      if (!response) {
-        range.insertNode(displayProgress());
-      } else if (!response.ok) {
-        range.insertNode(displayError(response.errors));
-      }
-    };
-
-    function displayProgress(): Element {
-      return document.createElement('conduit-loader');
-    }
-
-    function displayError(errors: ApiResponse.Errors): Element {
-
-      const errorList = errorGen(errors);
-
-      if (errorList) {
-        return errorList;
-      }
-
-      const loader = document.createElement('conduit-loader');
-
-      loader.setAttribute('load-error', 'Failed to load articles');
-
-      return loader;
-    }
-
   }
 
 }
---
+++ src/pages/home/render-feed.decorator.ts
@@ -0,0 +1,152 @@
+import { HierarchyContext } from '@wesib/generic';
+import {
+  BootstrapWindow,
+  ComponentClass,
+  ComponentContext,
+  ComponentDef,
+  ComponentProperty,
+  ComponentPropertyDecorator,
+  ElementRenderer,
+  Render,
+} from '@wesib/wesib';
+import { ContextKey, ContextKey__symbol, SingleContextKey } from 'context-values';
+import { nextOnEvent, StatePath, trackValue } from 'fun-events';
+import { ApiResponse } from '../../common/api';
+import { ArticleList, FeedRequest, feedRequestsEqual, FeedService } from '../../common/feed';
+import { ApiErrorGenerator } from '../../common/input';
+import { ArticleListComponent } from './article-list.component';
+import { FeedArticleList } from './feed-article-list';
+import { FeedPagerComponent } from './feed-pager.component';
+
+const RenderFeedState__key = (/*#__PURE__*/ new SingleContextKey<RenderFeedState>('render-feed-state'));
+const RenderFeedState__symbol = (/*#__PURE__*/ Symbol('render-feed-state'));
+
+class RenderFeedState {
+
+  static get [ContextKey__symbol](): ContextKey<RenderFeedState> {
+    return RenderFeedState__key;
+  }
+
+  readonly response = trackValue<ApiResponse<ArticleList>>();
+  private readonly _request = trackValue<FeedRequest>({});
+
+  constructor(
+      context: ComponentContext,
+      path: StatePath,
+  ) {
+
+    const feedService = context.get(FeedService);
+
+    this.response.on((newResponse, oldResponse) => context.updateState(path, newResponse, oldResponse));
+    context.get(HierarchyContext).provide({
+      a: FeedArticleList,
+      is: this.response.read.keep.thru_(
+          response => response?.ok
+              ? response.body
+              : { articles: [], articlesCount: 0 },
+      ),
+    });
+    context.whenOn(supply => {
+      this._request.read
+          .tillOff(supply)
+          .thru_(
+              request => {
+                this.response.it = undefined;
+                return nextOnEvent(feedService.articles(request));
+              },
+          )(response => this.response.it = response);
+    });
+  }
+
+  get request(): FeedRequest {
+    return this._request.it;
+  }
+
+  set request(value: FeedRequest) {
+
+    const prevRequest = this.request;
+
+    if (!feedRequestsEqual(value, prevRequest)) {
+      this._request.it = value;
+    }
+  }
+
+}
+
+export function RenderFeed<T extends ComponentClass>(): ComponentPropertyDecorator<FeedRequest, T> {
+  return ComponentProperty(({ get, set: setValue, key }) => {
+
+    const path: StatePath = [RenderFeedState__symbol, key];
+
+    return {
+      componentDef: ComponentDef.all(
+          {
+            feature: {
+              needs: [
+                ArticleListComponent,
+                FeedPagerComponent,
+              ],
+            },
+            define(defContext) {
+              defContext.perComponent({
+                a: RenderFeedState,
+                by: (context: ComponentContext) => new RenderFeedState(context, path),
+              });
+            },
+          },
+          Render({ path }).As(renderFeed, key),
+      ),
+      get,
+      set(component, value) {
+        setValue(component, value);
+        ComponentContext.of(component).get(RenderFeedState).request = value;
+      },
+    };
+
+    function renderFeed(this: object): ElementRenderer {
+
+      const context = ComponentContext.of(this);
+      const { contentRoot }: { contentRoot: Node } = context;
+      const document = context.get(BootstrapWindow).document;
+      const errorGen = context.get(ApiErrorGenerator);
+      const list = contentRoot.appendChild(document.createElement('conduit-article-list'));
+      const pager = contentRoot.appendChild(document.createElement('conduit-feed-pager'));
+      const range = document.createRange();
+      const state = context.get(RenderFeedState);
+
+      range.setStartAfter(list);
+      range.setEndBefore(pager);
+
+      return () => {
+        range.deleteContents();
+
+        const response = state.response.it;
+
+        if (!response) {
+          range.insertNode(displayProgress());
+        } else if (!response.ok) {
+          range.insertNode(displayError(response.errors));
+        }
+      };
+
+      function displayProgress(): Element {
+        return document.createElement('conduit-loader');
+      }
+
+      function displayError(errors: ApiResponse.Errors): Element {
+
+        const errorList = errorGen(errors);
+
+        if (errorList) {
+          return errorList;
+        }
+
+        const loader = document.createElement('conduit-loader');
+
+        loader.setAttribute('load-error', 'Failed to load articles');
+
+        return loader;
+      }
+    }
+  });
+}
