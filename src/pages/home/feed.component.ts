a6e17d0e1023035b21aaf743cd4a687c95922f4c
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-02-28 17:26:00 +0700

    Remove redundant dep

--- src/pages/home/feed.component.ts
+++ src/pages/home/feed.component.ts
@@ -2,22 +2,10 @@ import { Navigation } from '@wesib/generic';
 import { Component, ComponentContext } from '@wesib/wesib';
 import { Conduit__NS } from '../../common';
 import { FeedRequest } from '../../common/feed';
-import { ArticleListComponent } from './article-list.component';
-import { FeedPagerComponent } from './feed-pager.component';
 import { PageFeedParam } from './page-feed-param';
 import { RenderFeed } from './render-feed.decorator';
 
-@Component(
-    ['feed', Conduit__NS],
-    {
-      feature: {
-        needs: [
-          ArticleListComponent,
-          FeedPagerComponent,
-        ],
-      },
-    },
-)
+@Component(['feed', Conduit__NS])
 export class FeedComponent {
 
   @RenderFeed()
