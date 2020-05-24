89acac0e7cbcdb450b01de2fe540a00c271e63de
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-05-24 16:30:46 +0700

    Less on `fs-extra` less

--- build/rollup-plugin-cleanup.js
+++ build/rollup-plugin-cleanup.js
@@ -1,24 +1,21 @@
+import { promises as fs } from 'fs';
 import glob from 'glob';
-import fs from 'fs-extra';
+import { promisify } from 'util';
 
 export default function cleanup(what) {
 
-  function clear() {
-    return new Promise((resolve, reject) => glob(what, (err, files) => {
-      if (err) {
-        reject(err);
-      } else if (!files.length) {
-        resolve(null);
-      } else {
-        console.log(`Removing files: ${files.join(', ')}`);
-        resolve(Promise.all(files.map(file => fs.remove(file))).then(() => null));
-      }
-    }));
+  async function clear() {
+
+    const files = await promisify(glob)(what);
+
+    console.log('Removing files', ...files);
+    await Promise.all(files.map(
+        file => fs.unlink(file),
+    ));
   }
 
   return {
     name: 'cleanup',
     buildStart: clear,
-    load: clear,
   };
 }
