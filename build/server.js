53be83a0297be78e0ce9ee682170268cbdb1af48
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-11-08 17:17:30 +0700

    Hatsy v2

--- build/server.js
+++ build/server.js
@@ -1,11 +1,39 @@
 import { httpListener } from '@hatsy/hatsy';
+import { ZLogging } from '@hatsy/log-z-request';
 import { Routing } from '@hatsy/router';
 import { serveStatic } from '@hatsy/serve-static';
+import { logZAtopOf, logZTimestamp, logZWithDetails } from '@run-z/log-z';
+import { logZToStream } from '@run-z/log-z/node';
 import { createServer } from 'http';
 import path from 'path';
 
 const server = createServer(httpListener(
-    Routing.for(serveStatic(path.join(process.cwd(), 'dist'))),
+    {
+
+      handleBy(handler) {
+        return ZLogging.with({
+
+          by: logZTimestamp(
+              logZToStream(process.stdout),
+          ),
+
+          forRequest(logger, { request: { method, url } }) {
+            return logZWithDetails(
+                {
+                  method,
+                  url,
+                },
+                logZAtopOf(logger),
+            );
+          },
+
+        }).for(handler);
+      },
+
+    },
+    Routing.for(
+        serveStatic(path.join(process.cwd(), 'dist')),
+    ),
 ));
 
 server.listen(4200);
