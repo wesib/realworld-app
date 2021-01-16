61f7ff9052420f3f912a0154e1d4ab2a5cb79e6d
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-01-16 15:13:14 +0700

    Utilize component slot

--- src/core/util/render-now.ts
+++ src/core/util/render-now.ts
@@ -1,10 +1,9 @@
 import { HierarchyContext } from '@wesib/generic';
-import { ComponentContext, ComponentContextHolder, ElementRenderCtl } from '@wesib/wesib';
+import { ComponentContext, ComponentElement, ComponentSlot, ElementRenderCtl } from '@wesib/wesib';
 
-export function renderNow(element: Element & ComponentContextHolder, enclosing: ComponentContext): void {
-
-  const context = ComponentContext.of(element);
-
-  context.get(HierarchyContext).inside(enclosing);
-  context.get(ElementRenderCtl).renderNow();
+export function renderNow(element: ComponentElement, enclosing: ComponentContext): void {
+  ComponentSlot.of(element).whenReady(context => {
+    context.get(HierarchyContext).inside(enclosing);
+    context.get(ElementRenderCtl).renderNow();
+  });
 }
